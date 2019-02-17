8483ae6e181c277fe493cd4030c20b9ea7a37e21
dybl <dyblcjj@163.com>
2019-02-18 00:11:28 +0800

    delete android dir

---
+++ face4d/android/app/src/main/java/com/junjc9/funu/app/face4d/ui/activity/MainActivity.java
@@ -0,0 +1,154 @@
+package com.junjc9.funu.app.face4d.ui.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Display;
+import android.view.MotionEvent;
+import android.view.WindowManager;
+import android.content.Context;
+
+import com.junjc9.funu.app.face4d.R;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    private CoordinatorLayout right;
+    private NavigationView left;
+    private boolean isDrawer=false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, getResources().getString(R.string.nav_header_subtitle), Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        right = (CoordinatorLayout) findViewById(R.id.right);
+        left = (NavigationView) findViewById(R.id.nav_view);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        right.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if(isDrawer){
+                    return left.dispatchTouchEvent(motionEvent);
+                }else{
+                    return false;
+                }
+            }
+        });
+        drawer.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                isDrawer=true;
+                //获取屏幕的宽高
+                WindowManager manager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+                Display display = manager.getDefaultDisplay();
+                //设置右面的布局位置  根据左面菜单的right作为右面布局的left   左面的right+屏幕的宽度（或者right的宽度这里是相等的）为右面布局的right
+                right.layout(left.getRight(), 0, left.getRight() + display.getWidth(), display.getHeight());
+            }
+            @Override
+            public void onDrawerOpened(View drawerView) {}
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                isDrawer=false;
+            }
+            @Override
+            public void onDrawerStateChanged(int newState) {}
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        } else if (id == R.id.action_open_in_browser) {
+            return true;
+        } else if (id == R.id.action_help_and_feedback) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        } else if (id == R.id.nav_about) {
+
+        } else if (id == R.id.nav_setting) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ face4d/android/app/src/main/java/com/junjc9/funu/app/face4d/ui/activity/SplashActivity.java
@@ -0,0 +1,63 @@
+package com.junjc9.funu.app.face4d.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import com.junjc9.funu.app.face4d.R;
+import com.junjc9.funu.app.face4d.widget.view.RoundImageView;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash); //内容视图是hello.xml
+        Button bt= (Button) findViewById(R.id.splash_bt); //获取按钮在xml中的id
+        bt.setOnClickListener(new OnClickListener() {	//bt按钮的点击事件
+            @Override
+            public void onClick(View arg0) {  //重载onClick方法
+                Intent intent=new Intent();  //实例化Intnent对象
+                intent.setClass(SplashActivity.this,MainActivity.class); //设置HelloActivity.class,MainActivity.class两个类
+                SplashActivity.this.startActivity(intent); //启动MainActivity.class
+            }
+        });
+        RoundImageView splash_img = (RoundImageView) findViewById(R.id.splash_img); //获取圆形图像视图在xml中的id
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.ic_hello); //通过xml的路径静态调用mipmap中的图片资源
+        splash_img.setBitmap(bitmap); //显示位图
+
+/*
+    private final int SPLASH_DISPLAY_LENGHT = 3000;
+    private Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash);
+
+        RoundImageView splash_img = (RoundImageView) findViewById(R.id.splash_img);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.mipmap.ic_splash);
+        splash_img.setBitmap(bitmap);
+
+        handler = new Handler();
+        // 延迟SPLASH_DISPLAY_LENGHT时间然后跳转到MainActivity
+        handler.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,
+                        MainActivity.class);
+                startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGHT);
+*/
+
+
+    }
+}
