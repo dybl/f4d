686a7c7856910b8e875f1b0695bed671bce88ff2
dybl <dyblcjj@163.com>
2019-03-13 23:46:21 +0800

    trivia commit for add -

--- src/f4d.cpp
+++ src/f4d.cpp
@@ -16,25 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/*
- * eos - A 3D Morphable Model fitting library written in modern C++11/14.
- *
- * File: eos-model-viewer.cpp
- *
- * Copyright 2017, 2018 Patrik Huber
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 #include "cxxopts.hpp"
 
 #include "eos/core/Mesh.hpp"
@@ -154,13 +136,13 @@ int main(int argc, const char* argv[])
     string model_file, blendshapes_file;
     try
     {
-        cxxopts::Options options("eos-model-viewer", "OpenGL viewer for eos's 3D morphable models.");
+        cxxopts::Options options("face4d-demo", "OpenGL viewer for eos's 3D morphable models.");
         // clang-format off
         options.add_options()
-            ("h,help", "display the help message")
-            ("m,model", "an eos 3D Morphable Model stored as cereal BinaryArchive (.bin)",
+            ("-h,help", "display the help message")
+            ("-m,model", "an eos 3D Morphable Model stored as cereal BinaryArchive (.bin)",
                 cxxopts::value(model_file))
-            ("b,blendshapes", "an eos file with blendshapes (.bin)",
+            ("-b,blendshapes", "an eos file with blendshapes (.bin)",
                 cxxopts::value(blendshapes_file));
         // clang-format on
         const auto result = options.parse(argc, argv);
